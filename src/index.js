3ccee0a1aeca3502f60ae5ddb3352f95155adf6d
ange <angecide@gmail.com>
2023-05-29 11:41:38 +0200

    created initial test setup for testing only rendering one button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Main } from './js/Components';
 import './css/style.css';
-import { Board } from './js/Components';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <Board />
+    <Main />
   </StrictMode>
 );
--- src/js/Components.js
+++ src/js/Components.js
@@ -1,5 +1,34 @@
-export function Board() {
+import { useState } from 'react'
+
+/*
+simple test script:
+create three rows with a button, using map function to genereate the div
+if I press on one of the three buttons, it should draw "╳" on that button
+the crucial part is: only that button should be re-rendered, and nothing else
+*/
+function Board() {
+    const [state, setState] = useState("")
+    function eventHandler(e) {
+        console.log(e)
+        console.log(e.target)
+    }
     return (
-        <div className="header">{"Ultimate Tic Tac Toe"}</div>
+        [1, 2, 3].map((i) =>
+            <div className="board" key={i}>
+                <button className="square" key={i + 3} onClick={eventHandler}>
+                    {state}
+                </button>
+            </div>
+        )
+    )
+}
+
+
+export function Main() {
+    return (
+        <>
+            <div className="header">{"Ultimate Tic Tac Toe"}</div>
+            <Board/>
+        </>
     )
 }
