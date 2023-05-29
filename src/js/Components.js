4e3375544fd1af51822c62d4b4b3dc02d61c7bf8
ange <angecide@gmail.com>
2023-05-29 12:05:24 +0200

    changing the props directly seems to render component without anything else getting re-rendered

--- src/js/Components.js
+++ src/js/Components.js
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 /*
 simple test script:
 create three rows with a button, using map function to genereate the div
@@ -7,22 +5,14 @@ if I press on one of the three buttons, it should draw "╳" on that button
 the crucial part is: only that button should be re-rendered, and nothing else
 */
 function Board() {
-    const [state, setState] = useState("")
-    function eventHandler(e) {
-        console.log(e)
-        console.log(e.target)
-    }
     return (
         [1, 2, 3].map((i) =>
             <div className="board" key={i}>
-                <button className="square" key={i + 3} onClick={eventHandler}>
-                    {state}
-                </button>
+                <button className="square" key={i + 3} onClick={e => e.target.innerText = "╳"}/>
             </div>
         )
     )
 }
-
 
 export function Main() {
     return (
