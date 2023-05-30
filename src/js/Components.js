ef100c43f2961466fa464283a01528531fa9f99c
ange <angecide@gmail.com>
2023-05-30 07:18:37 +0200

    instead of updating a button text directly using e.target, I decide to just use useState for each button

--- src/js/Components.js
+++ src/js/Components.js
@@ -1,14 +1,19 @@
-/*
-simple test script:
-create three rows with a button, using map function to genereate the div
-if I press on one of the three buttons, it should draw "╳" on that button
-the crucial part is: only that button should be re-rendered, and nothing else
-*/
+import { useState } from 'react'
+
+function Square() {
+    const [state, setState] = useState(false)
+    return (
+        <button className='square' onClick={() => setState(!state)}>
+            {state ? "╳" : ""}
+        </button>
+    )
+}
+
 function Board() {
     return (
         [1, 2, 3].map((i) =>
             <div className="board" key={i}>
-                <button className="square" key={i + 3} onClick={e => e.target.innerText = "╳"}/>
+                <Square />
             </div>
         )
     )
@@ -18,7 +23,7 @@ export function Main() {
     return (
         <>
             <div className="header">{"Ultimate Tic Tac Toe"}</div>
-            <Board/>
+            <Board />
         </>
     )
 }
