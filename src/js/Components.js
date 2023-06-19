694031ce3eec7a53a5acec1eaa5fb081381c8681
ange <angecide@gmail.com>
2023-06-19 22:35:18 +0200

    auto formatting

--- src/js/Components.js
+++ src/js/Components.js
@@ -5,11 +5,14 @@ let board_array = Array(81).fill("")
 function Square(props) {
     const [state, setState] = useState(true)
     const update = () => {
-        board_array[props.idx] = state ? "╳": "◯"
+        board_array[props.idx] = state ? "╳" : "◯"
         setState(!state)
     }
     return (
-        <button className='square' onClick={() => update()}>
+        <button
+            className='square'
+            onClick={() => update()}
+        >
             {board_array[props.idx]}
         </button>
     )
@@ -21,9 +24,9 @@ function MiniBoard(props) {
             {
                 [0, 3, 6].map((i) =>
                     <div className='minirow' key={i}>
-                        <Square idx={i + props.board_idx}/>
-                        <Square idx={i + 1 + props.board_idx}/>
-                        <Square idx={i + 2 + props.board_idx}/>
+                        <Square idx={i + props.board_idx} />
+                        <Square idx={i + 1 + props.board_idx} />
+                        <Square idx={i + 2 + props.board_idx} />
                     </div>
                 )
             }
@@ -34,9 +37,9 @@ function MiniBoard(props) {
 function Row(props) {
     return (
         <div className={props.name}>
-            <MiniBoard name={"leftboard"} board_idx={0 + props.row_idx}/>
-            <MiniBoard name={"middleboard"} board_idx={9 + props.row_idx}/>
-            <MiniBoard name={"rightboard"} board_idx={18 + props.row_idx}/>
+            <MiniBoard name={"leftboard"} board_idx={0 + props.row_idx} />
+            <MiniBoard name={"middleboard"} board_idx={9 + props.row_idx} />
+            <MiniBoard name={"rightboard"} board_idx={18 + props.row_idx} />
         </div>
     )
 }
@@ -44,9 +47,9 @@ function Row(props) {
 export function Board() {
     return (
         <>
-            <Row name={"upperrow"} row_idx={0}/>
-            <Row name={"middlerow"} row_idx={27}/>
-            <Row name={"lowerrow"} row_idx={54}/>
+            <Row name={"upperrow"} row_idx={0} />
+            <Row name={"middlerow"} row_idx={27} />
+            <Row name={"lowerrow"} row_idx={54} />
         </>
     )
 }
