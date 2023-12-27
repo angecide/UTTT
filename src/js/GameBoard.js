5f51430ff4150c3c2be58786506515585e77b8ef
ange <angecide@gmail.com>
2023-12-27 22:52:30 +0100

    variable name changes

--- src/js/GameBoard.js
+++ src/js/GameBoard.js
@@ -4,38 +4,38 @@ import {ResetButtons} from './ResetButtons';
 export function Board() {
     const [board_array, set_board_array] = useState(Array(81).fill(""))
 
-    function Square({idx}) {
+    function Square({square_idx}) {
         const [state, updateState] = useState(true)
         const update_square = () => {
-            board_array[idx] = state ? "╳" : "◯"
+            board_array[square_idx] = state ? "╳" : "◯"
             updateState(!state)
         }
         return <button className="square" onClick={() => update_square()}>
-            {board_array[idx]}
+            {board_array[square_idx]}
         </button>
     }
 
-    function MiniRow({row_idx, board_idx}) {
+    function MiniRow({mini_row_idx}) {
         return <div className={"mini-row"}>
-            <Square idx={row_idx + board_idx}/>
-            <Square idx={row_idx + 1 + board_idx}/>
-            <Square idx={row_idx + 2 + board_idx}/>
+            <Square square_idx={mini_row_idx}/>
+            <Square square_idx={mini_row_idx + 1}/>
+            <Square square_idx={mini_row_idx + 2}/>
         </div>
     }
 
-    function MiniBoard({name, ...board_idx}) {
+    function MiniBoard({name, board_idx}) {
         return <div className={name}>
-            <MiniRow row_idx={0} {...board_idx} />
-            <MiniRow row_idx={3} {...board_idx} />
-            <MiniRow row_idx={6} {...board_idx} />
+            <MiniRow mini_row_idx={board_idx}/>
+            <MiniRow mini_row_idx={board_idx + 3}/>
+            <MiniRow mini_row_idx={board_idx + 6}/>
         </div>
     }
 
     function Row({name, row_idx}) {
         return <div className={name}>
-            <MiniBoard name={"left-board"} board_idx={0 + row_idx}/>
-            <MiniBoard name={"middle-board"} board_idx={9 + row_idx}/>
-            <MiniBoard name={"right-board"} board_idx={18 + row_idx}/>
+            <MiniBoard name={"left-board"} board_idx={row_idx}/>
+            <MiniBoard name={"middle-board"} board_idx={row_idx + 9}/>
+            <MiniBoard name={"right-board"} board_idx={row_idx + 18}/>
         </div>
     }
 
