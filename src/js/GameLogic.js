6ceb176e1c3765dfbfa719e16f893f64c916a0bf
ange <angecide@gmail.com>
2024-02-23 22:00:31 +0100

    removed the previously_disabled_squares and instead use legal moves

--- src/js/GameLogic.js
+++ src/js/GameLogic.js
@@ -1,12 +1,17 @@
 ﻿import Set from 'core-js-pure/actual/set';
 import range from 'core-js-pure/full/iterator/range';
 
-const entire_board_indices = new Set(range(0, 81)) // index for every square on the board
+const entire_board_indices = new Set(range(0, 81)) // used to quickly determine which squares to disable
 
-export const get_squares_to_disable = (move_played, disabled_squares) => {
+export function get_squares_to_enable_and_disable(move_played, disabled_squares) {
     const start = 9 * (move_played % 9)
 
-    return entire_board_indices
-        .difference(new Set(range(start, start + 9))) // make sure the legal moves don't get disabled
-        .difference(disabled_squares) // no need to run setDisable on squares that have already been disabled
+    const legal_moves = new Set(range(start, start + 9))
+        .difference(disabled_squares) // make sure we don't enable squares that have already been played on
+
+    const squares_to_disable = entire_board_indices
+        .difference(disabled_squares) // no need to try to disable squares that have already been disabled
+        .difference(legal_moves) // we don't want to disable the legal moves
+
+    return {squares_to_enable: legal_moves, squares_to_disable: squares_to_disable}
 }
--- src/js/Main.js
+++ src/js/Main.js
@@ -2,39 +2,36 @@ import Set from 'core-js-pure/actual/set';
 import {useState} from 'react';
 import {ResetButtons} from './ResetButtons';
 import {Board} from "./Board";
-import {get_squares_to_disable} from "./GameLogic";
+import {get_squares_to_enable_and_disable} from "./GameLogic";
 
 export function Main() {
     const [board_array, set_board_array] = useState(Array(81).fill(""))
     const [start_turn, set_start_turn] = useState(0)
 
-    const turn_map = {"1": "✕", "-1": "〇", "0": ""} // map to determine which symbol to draw based on whose turn it is
-    let current_turn = start_turn // track whose turn it is currently
+    const turn_symbol_map = {"1": "✕", "-1": "〇", "0": ""}
+    let current_turn = start_turn // track whose turn it is
 
-    let setDisable_collection = Array(81) // used to selectively disable squares, without re-rendering the other squares
+    let setDisable_collection = Array(81) // used to selectively disable squares
     let occupied_squares = new Set([]) // collection of square_idx that have already been played, no need to enable/disable these again
-    let previously_disabled_squares = new Set([]) // used to re-enable all the non-occupied squares that were disabled previously
 
     function Square({square_idx}) {
         const [state, updateState] = useState("")
         const [disable, setDisable] = useState(false)
         setDisable_collection[square_idx] = setDisable
 
-        const disable_squares = (new_squares_to_disable) => {
-            new_squares_to_disable
-                .forEach(e => setDisable_collection[e](true))
-            previously_disabled_squares
-                .difference(occupied_squares)
-                .difference(new_squares_to_disable)
+        const enable_and_disable_squares = () => {
+            const {squares_to_enable, squares_to_disable} = get_squares_to_enable_and_disable(square_idx, occupied_squares)
+            squares_to_enable
                 .forEach(e => setDisable_collection[e](false))
-            previously_disabled_squares = new_squares_to_disable
+            squares_to_disable
+                .forEach(e => setDisable_collection[e](true))
         }
 
         const update_square = () => {
             board_array[square_idx] = current_turn
-            updateState(turn_map[current_turn])
+            updateState(turn_symbol_map[current_turn])
             current_turn = -current_turn
-            disable_squares(get_squares_to_disable(square_idx, occupied_squares))
+            enable_and_disable_squares()
             occupied_squares.add(square_idx) // by adding this now instead of earlier, we ensure that the previous function disables square_idx
         }
 
