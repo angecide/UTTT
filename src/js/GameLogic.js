b0120ee2c9e71a716f67ff6afb915d020b3c7b2a
ange <angecide@gmail.com>
2024-02-23 23:20:31 +0100

    created function called evaluate_TTT_board, which will contain further game logic

--- src/js/GameLogic.js
+++ src/js/GameLogic.js
@@ -3,10 +3,19 @@ import range from 'core-js-pure/full/iterator/range';
 
 const entire_board_indices = new Set(range(0, 81)) // used to quickly determine which squares to disable
 
+function evaluate_TTT_board(move_played, board_array) {
+    /*
+    based on the move that was played and the resulting board state, figure out whether or not the board has been won or
+    drawn, just like in regular TTT, and then disable all those squares and color the TTT board with the winner's color.
+
+    At some point I also need to figure out a way to evaluate the bigger TTT board to evaluate who won the overall game.
+     */
+}
+
 export function evaluate_legal_moves(move_played, disabled_squares) {
     const start = 9 * (move_played % 9) // start index of the corresponding TTT board based on move_played
 
-    const legal_moves = new Set(range(start, start + 9))
+    const legal_moves = new Set(range(start, start + 9)) // this range encompasses the indexes of the TTT board
         .difference(disabled_squares) // make sure we don't enable squares that have already been played on
 
     const squares_to_disable = entire_board_indices
--- src/js/Main.js
+++ src/js/Main.js
@@ -11,20 +11,18 @@ export function Main() {
     const turn_symbol_map = {"1": "✕", "-1": "〇", "0": ""}
     let current_turn = start_turn // track whose turn it is
 
-    let setDisable_collection = Array(81) // used to selectively disable squares
+    let setDisable_collection = Array(81) // used to selectively enable or disable squares
     let occupied_squares = new Set([]) // collection of square_idx that have already been played
 
     function Square({square_idx}) {
         const [state, updateState] = useState("")
-        const [disable, setDisable] = useState(false)
+        const [disable, setDisable] = useState(false) // TODO: disable all squares if start_turn === 0
         setDisable_collection[square_idx] = setDisable
 
         const enable_and_disable_squares = () => {
             const {squares_to_enable, squares_to_disable} = evaluate_legal_moves(square_idx, occupied_squares)
-            squares_to_enable
-                .forEach(e => setDisable_collection[e](false))
-            squares_to_disable
-                .forEach(e => setDisable_collection[e](true))
+            squares_to_enable.forEach(e => setDisable_collection[e](false))
+            squares_to_disable.forEach(e => setDisable_collection[e](true))
         }
 
         const update_square = () => {
