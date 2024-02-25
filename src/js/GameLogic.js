007e6a4e43f88e816aab75382984332e136d0c0f
ange <angecide@gmail.com>
2024-02-25 23:14:39 +0100

    renamed evaluate_legal_moves into update_game_state which also handles disabling/enabling squares

--- src/js/GameLogic.js
+++ src/js/GameLogic.js
@@ -4,31 +4,33 @@ import range from 'core-js-pure/full/iterator/range';
 const entire_board_indices = new Set(range(0, 81)) // used to quickly determine which squares to disable
 
 const winning_line = [292, 448, 273, 84, 73, 56, 7, 146]
-const board_is_done = range(0, 512)
-
-export function update_bit_array(current_turn, move_played, player_bit_arrays) {
-    /*
-    based on the move that was played and the resulting board state, figure out whether or not the board has been won or
-    drawn, just like in regular TTT, and then disable all those squares and color the TTT board with the winner's color.
-
-    At some point I also need to figure out a way to evaluate the bigger TTT board to evaluate who won the overall game.
-     */
-
-    const board = Math.floor(move_played / 9)
-    const move = move_played % 9
-
+const board_is_done = new Array(512).fill(false)
+for (const board of range(0, 512)) {
+    for (const line of winning_line) {
+        if ((board & line) === line) {
+            board_is_done[board] = true
+        }
+    }
 }
 
-export function evaluate_legal_moves(move_played, disabled_squares) {
-    const start = 9 * (move_played % 9) // start index of the square to the TTT board consisting of legal moves
+export function update_game_state({player_bit_arrays, move_played, current_turn, disabled_squares, setDisables}) {
+    const board = Math.floor(move_played / 9) // current board index of move_played
+    const move = move_played % 9 // the corresponding move_idx relative to the board
+    const start = 9 * move // the starting index on the TTT that the next player has to play on based on move_played
 
-    const legal_moves = new Set(range(start, start + 9))
+    player_bit_arrays[current_turn][board] += (1 << move)
+    if (board_is_done[player_bit_arrays[current_turn][board]]) {
+        player_bit_arrays[current_turn][9] += (1 << board)
+    }
+
+    const squares_to_enable = new Set(range(start, start + 9))
         .difference(disabled_squares) // make sure we don't enable squares that have already been played on
         .difference(new Set([move_played])) // in case move_played points to the same TTT board
 
     const squares_to_disable = entire_board_indices
         .difference(disabled_squares) // no need to try to disable squares that have already been disabled
-        .difference(legal_moves) // we don't want to disable the legal moves
+        .difference(squares_to_enable) // we don't want to disable the squares we want to enable
 
-    return {squares_to_enable: legal_moves, squares_to_disable: squares_to_disable}
+    squares_to_enable.forEach(e => setDisables[e](false))
+    squares_to_disable.forEach(e => setDisables[e](true))
 }
--- src/js/Main.js
+++ src/js/Main.js
@@ -1,35 +1,31 @@
 import {useState} from 'react';
 import {ResetButtons} from './ResetButtons';
 import {Board} from "./Board";
-import {evaluate_legal_moves, update_bit_array} from "./GameLogic";
+import {update_game_state} from "./GameLogic";
 
 export function Main() {
     const [start_turn, set_start_turn] = useState(0)
 
     const turn_symbol_map = {"1": "✕", "-1": "〇", "0": ""}
-    let current_turn = start_turn // track whose turn it is
 
-    let setDisable_collection = Array(81) // used to selectively enable or disable squares
-    let occupied_squares = new Set([]) // collection of square_idx that have already been played
-    const player_bit_arrays = {"1": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], "-1": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
+    const game_state = { // player_bit_arrays tracks the moves played on the board by each player using bit arrays
+        player_bit_arrays: {"1": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "-1": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+        current_turn: start_turn, // track whose turn it is
+        disabled_squares: new Set([]), // collection of square_idx that have already been played
+        setDisables: new Array(81) // used to selectively enable or disable squares
+    }
 
     function Square({square_idx}) {
         const [state, updateState] = useState("")
         const [disable, setDisable] = useState(start_turn === 0)
-        setDisable_collection[square_idx] = setDisable
-
-        const enable_and_disable_squares = () => {
-            const {squares_to_enable, squares_to_disable} = evaluate_legal_moves(square_idx, occupied_squares)
-            squares_to_enable.forEach(e => setDisable_collection[e](false))
-            squares_to_disable.forEach(e => setDisable_collection[e](true))
-        }
+        game_state.setDisables[square_idx] = setDisable
 
         const update_square = () => {
-            updateState(turn_symbol_map[current_turn])
-            current_turn = -current_turn
-            update_bit_array(current_turn, square_idx, player_bit_arrays)
-            enable_and_disable_squares()
-            occupied_squares.add(square_idx)
+            updateState(turn_symbol_map[game_state.current_turn])
+            game_state.move_played = square_idx
+            update_game_state(game_state)
+            game_state.disabled_squares.add(square_idx)
+            game_state.current_turn = -game_state.current_turn
         }
 
         return <button className="square"
