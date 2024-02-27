b5c1527a33d9a8f82400621dc6454ce3f72befb9
ange <angecide@gmail.com>
2024-02-27 23:10:55 +0100

    WIP towards enforcing complete UTTT rules regarding enabling and disabling squares

--- src/js/GameLogic.js
+++ src/js/GameLogic.js
@@ -3,7 +3,7 @@ import range from 'core-js-pure/full/iterator/range';
 
 const entire_board_indices = new Set(range(0, 81)) // used to quickly determine which squares to disable
 
-const draw = 511
+const filled = 511
 const winning_lines = [292, 448, 273, 84, 73, 56, 7, 146]
 const board_won = new Array(512).fill(false) // used to quickly determine if a board has been won
 for (const board of range(0, 512)) { // 0 to 511 are basically all the possible combinations of TTT boards
@@ -29,25 +29,34 @@ export function update_game_state({player_bit_arrays, move_played, current_turn,
         if (board_won[current_board[9]]) { // check if the big board has been won
             console.log(current_turn, "won the game by playing", move_played, "that player's bit array:", current_board)
         }
-    } else if ((current_board[board] | other_board[board]) === draw) { // check if the board is drawn
+    } else if ((current_board[board] | other_board[board]) === filled) { // check if the board has been drawn
         player_bit_arrays[0] |= (1 << board) // update the draw board with the information that this board has been drawn
         console.log(current_turn, "draw on board", board, "by playing", move_played, "players bit arrays:", player_bit_arrays)
     }
-    if (((current_board[9] | other_board[9] | player_bit_arrays[0]) & draw) === draw) // check if the big board is drawn
-    {
+
+    const big_board = (current_board[9] | other_board[9] | player_bit_arrays[0])^filled // flip the zeros and ones
+    if (big_board === 0) { // check if the big board has been drawn
+
         console.log("the game ends in a draw")
     }
 
-    const start = move * 9 // the starting index of the TTT board where the next player has to play based on move_played
+    const squares_to_enable = new Set([])
+    if (big_board & (1 << move)) {
+        const start = move * 9
+        squares_to_enable.union(new Set(range(start, start + 9)))
+        for (const item of squares_to_enable){console.log(item)}
+    } else {
+        squares_to_enable
+            .union(entire_board_indices)
+    }
 
-    const squares_to_enable = new Set(range(start, start + 9))
-        .difference(disabled_squares) // make sure we don't enable squares that have already been played on
-        .difference(new Set([move_played])) // in case move_played points to the same TTT board
+    squares_to_enable
+        .difference(disabled_squares)
+        .difference(new Set([move_played]))
+    squares_to_enable.forEach(e => setDisables[e](false))
 
     entire_board_indices
-        .difference(disabled_squares) // no need to try to disable squares that have already been disabled
-        .difference(squares_to_enable) // we don't want to disable the squares we want to enable
+        .difference(squares_to_enable)
+        .difference(disabled_squares)
         .forEach(e => setDisables[e](true))
-
-    squares_to_enable.forEach(e => setDisables[e](false))
 }
